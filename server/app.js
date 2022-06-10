5c6e7caaa8cbccf225471de9b267d0eb9d1b12d2
harsh <solankiharsh462@gmail.com>
2022-06-10 15:01:13 +0530

    first

---
+++ server/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const connectDB = require('./db/connect')
+const cookieParser = require('cookie-parser')
+// const userRoutes = require('./routes/userRoutes')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+//middlewares
+
+//routes
+// app.use('/user',userRoutes)
+
+// Connect to mongodb
+const PORT = process.env.PORT || 5000
+const URI = process.env.MONGODB_URL
+const start = async () => {
+    try {
+        await connectDB(URI)
+        app.listen(PORT, () => {
+        console.log('Server is running on port',PORT)
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
